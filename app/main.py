f59ac64d634f095846b635596823937507f48f2b
Jorge Rabling <j.rabling@gmail.com>
2025-07-08 09:22:40 -0600

    create main

--- app/main.py
+++ app/main.py
@@ -1,12 +1,17 @@
-# app/main.py
 from fastapi import FastAPI
 from app.database import engine
 from app.models import Base
+from app.routes import categories, products, users, orders
 
 app = FastAPI(title="E-commerce API")
 
 Base.metadata.create_all(bind=engine)
 
+app.include_router(categories.router)
+app.include_router(products.router)
+app.include_router(users.router)
+app.include_router(orders.router)
+
 @app.get("/")
-def read_root():
-    return {"msg": "Bienvenido a la API de E-commerce"}
+def root():
+    return {"msg": "API de E-commerce funcionando"}
